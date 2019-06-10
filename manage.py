7b4e34e1f5ed5f2ce64a8b30709eab02e3387d69
JO <rhsnqk@daum.net>
2019-06-11 03:01:27 +0900

    firstproject

--- manage.py
+++ manage.py
@@ -1,14 +1,3 @@
-# Created by https://www.gitignore.io/api/django
-# Edit at https://www.gitignore.io/?templates=django
-
-### Django ###
-*.log
-*.pot
-*.pyc
-__pycache__/
-local_settings.py
-db.sqlite3
-media
 
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
